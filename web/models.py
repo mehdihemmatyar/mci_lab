87b9e7c9f431e5f17a4f0c3d2cd5e01eeb8069c8
mahdihemat <mahdihemat@hotmail.com>
2020-11-30 03:09:13 +0330

    implement post request

--- web/models.py
+++ web/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Token(models.Model):
+    """docstring for token."""
+    user = models.OneToOneField(User ,on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+    def __str__(self):
+        return "{}.token".format(self.user)
+
 class Expense(models.Model):
     """docstring for Expence."""
 
@@ -18,4 +25,4 @@ class Income(models.Model):
     amount = models.BigIntegerField()
     user = models.ForeignKey(User , on_delete=models.CASCADE)
     def __str__(self):
-        return "()-()".format(self.date , self.amount)
+        return "{}-{}".format(self.date , self.amount)
